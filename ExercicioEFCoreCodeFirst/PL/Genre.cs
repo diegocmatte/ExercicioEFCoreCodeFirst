2da7da00c8af6d6471588829ee1eb4eda0676d9f
diegocmatte <diegocmatte@gmail.com>
2018-08-22 21:20:37 -0300

    primeiro commit

---
+++ ExercicioEFCoreCodeFirst/PL/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ ExercicioEFCoreCodeFirst/PL/MovieContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    public class MovieContext :DbContext
+    {
+        public static readonly LoggerFactory FabricaLogger = new LoggerFactory(new[] { new ConsoleLoggerProvider((_, __) => true, true) });
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(FabricaLogger).UseSqlServer((@"Server=(localdb)\MSSQLLocalDB;Database=DBMovieCF;Trusted_Connection=True;"));
+        }
+    }
+}
