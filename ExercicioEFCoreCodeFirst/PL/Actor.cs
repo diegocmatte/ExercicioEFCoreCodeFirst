8be5bf8ef29553d1677cb6506cea5b888cb629a6
diegocmatte <diegocmatte@gmail.com>
2018-08-27 20:51:25 -0300

    ajustando classes e pacotes

---
+++ ExercicioEFCoreCodeFirst/PL/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    public class Actor
+    {
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public object ActorId { get; internal set; }
+    }
+}
---
+++ ExercicioEFCoreCodeFirst/PL/ActorMovie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    class ActorMovie
+    {
+        public object ActorId { get; set; }
+        public string Character { get; set; }
+        public int MovieId { get; set; }
+    }
+}
